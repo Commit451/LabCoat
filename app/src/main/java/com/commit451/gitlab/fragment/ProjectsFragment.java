6950110715fa2b021dc9de258cbef8d743edb7c8
John Carlson <jawnnypoo@gmail.com>
2016-01-12 19:46:34 -0600

    Null check around the adapter clear in ProjectsFragment

--- app/src/main/java/com/commit451/gitlab/fragment/ProjectsFragment.java
+++ app/src/main/java/com/commit451/gitlab/fragment/ProjectsFragment.java
@@ -289,7 +289,9 @@ public class ProjectsFragment extends BaseFragment {
     }
 
     public void searchQuery(String query) {
-        mProjectsAdapter.clearData();
+        if (mProjectsAdapter != null) {
+            mProjectsAdapter.clearData();
+        }
         mQuery = query;
         loadData();
     }
