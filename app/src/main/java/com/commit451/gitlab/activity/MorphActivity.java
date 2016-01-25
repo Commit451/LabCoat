72f9b5686641376d6d11eb03f0162dbc6378fdb0
TR4Android <t.r.altstidl@freenet.de>
2016-01-25 17:00:21 +0100

    Correctly apply accent color
    * TabLayout indicator
    * AccessDialog action buttons (by extending MaterialDialog)
    * EditTexts

--- app/src/main/java/com/commit451/gitlab/activity/MorphActivity.java
+++ app/src/main/java/com/commit451/gitlab/activity/MorphActivity.java
@@ -10,10 +10,12 @@ import android.view.View;
 import android.view.animation.AnimationUtils;
 import android.view.animation.Interpolator;
 
+import com.afollestad.appthemeengine.Config;
 import com.commit451.easel.Easel;
 import com.commit451.gitlab.R;
 import com.commit451.gitlab.transition.MorphDialogToFab;
 import com.commit451.gitlab.transition.MorphFabToDialog;
+import com.commit451.gitlab.util.AppThemeUtil;
 
 /**
  * Activity that morphs from a FAB. Make sure the view you want to morph has the view id R.id.root and
@@ -27,7 +29,7 @@ public class MorphActivity extends BaseActivity {
             throw new IllegalStateException("Cannot pass an empty view");
         }
         if (Build.VERSION.SDK_INT >= 21) {
-            int fabColor = Easel.getThemeAttrColor(this, R.attr.colorAccent);
+            int fabColor = Config.accentColor(this, AppThemeUtil.resolveThemeKey(this));
             int dialogColor = Easel.getThemeAttrColor(this, android.R.attr.windowBackground);
             setupSharedElementTransitionsFab(this, root,
                     fabColor,
