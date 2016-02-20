bb50ce223435275ff2e37f0cad98976a87aae175
Michi302 <michi302@michi302.de>
2016-02-20 14:46:04 +0100

    Lowercase all timestamps. Closes #112

--- app/src/main/java/com/commit451/gitlab/util/DateUtils.java
+++ app/src/main/java/com/commit451/gitlab/util/DateUtils.java
@@ -15,11 +15,13 @@ public class DateUtils {
 
     public static CharSequence getRelativeTimeSpanString(Context context, Date startTime) {
         Date now = new Date();
-        if (startTime.before(now) && now.getTime() - startTime.getTime() < android.text.format.DateUtils.SECOND_IN_MILLIS) {
+        if (Math.abs(now.getTime() - startTime.getTime()) < android.text.format.DateUtils.SECOND_IN_MILLIS) {
             return context.getString(R.string.just_now);
         }
+
         return android.text.format.DateUtils.getRelativeTimeSpanString(startTime.getTime(),
                 now.getTime(),
-                android.text.format.DateUtils.SECOND_IN_MILLIS);
+                android.text.format.DateUtils.SECOND_IN_MILLIS)
+                .toString().toLowerCase();
     }
 }
