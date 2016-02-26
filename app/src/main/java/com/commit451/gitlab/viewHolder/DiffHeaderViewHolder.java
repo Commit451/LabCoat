8a79b42ab9572d7864cd1245ec315e164d507559
TR4Android <t.r.altstidl@freenet.de>
2016-02-26 19:40:12 +0100

    Improve diff activity
    This now shows all the information available (author, time, title and
    message)

--- app/src/main/java/com/commit451/gitlab/viewHolder/DiffHeaderViewHolder.java
+++ app/src/main/java/com/commit451/gitlab/viewHolder/DiffHeaderViewHolder.java
@@ -4,10 +4,15 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.commit451.gitlab.R;
+import com.commit451.gitlab.api.GitLabClient;
 import com.commit451.gitlab.model.api.RepositoryCommit;
+import com.commit451.gitlab.transformation.CircleTransformation;
+import com.commit451.gitlab.util.DateUtils;
+import com.commit451.gitlab.util.ImageUtil;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -23,7 +28,11 @@ public class DiffHeaderViewHolder extends RecyclerView.ViewHolder {
         return new DiffHeaderViewHolder(view);
     }
 
-    @Bind(R.id.title) TextView mTitle;
+    @Bind(R.id.commit_author_image) ImageView mImageView;
+    @Bind(R.id.commit_author) TextView mAuthorView;
+    @Bind(R.id.commit_time) TextView mTimeView;
+    @Bind(R.id.commit_title) TextView mTitleView;
+    @Bind(R.id.commit_message) TextView mMessageView;
 
     public DiffHeaderViewHolder(View view) {
         super(view);
@@ -31,6 +40,29 @@ public class DiffHeaderViewHolder extends RecyclerView.ViewHolder {
     }
 
     public void bind(RepositoryCommit commit) {
-        mTitle.setText(commit.getTitle());
+        GitLabClient.getPicasso()
+                .load(ImageUtil.getAvatarUrl(commit.getAuthorEmail(), itemView.getResources().getDimensionPixelSize(R.dimen.image_size)))
+                .transform(new CircleTransformation())
+                .into(mImageView);
+
+        mAuthorView.setText(commit.getAuthorName());
+        mTimeView.setText(DateUtils.getRelativeTimeSpanString(itemView.getContext(), commit.getCreatedAt()));
+        mTitleView.setText(commit.getTitle());
+        String message = extractMessage(commit.getTitle(), commit.getMessage());
+        mMessageView.setText(message);
+        mMessageView.setVisibility(message.isEmpty() ? View.GONE : View.VISIBLE);
+    }
+
+    /**
+     * This extracts the trailing part of the title as it is displayed in the GitLab web interface
+     * (the commit message also contains the commit title)
+     */
+    private String extractMessage(String title, String message) {
+        if (title.endsWith("\u2026")) {
+            String trailing = message.substring(title.length() - 1);
+            return trailing.equals("\u2026") ? "" : ("\u2026" + trailing).trim();
+        } else {
+            return ""; // No trailing part
+        }
     }
 }
