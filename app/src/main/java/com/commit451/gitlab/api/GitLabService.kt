7f5fdc6d3dbd26f1e5b8401c3176a3ca98a8e8fc
Noah Prail <nprail@filiosoft.com>
2017-08-08 11:04:26 -0400

    Repo tree per_page now 100 due to API limitation and it is now a constant :wink:

--- app/src/main/java/com/commit451/gitlab/api/GitLabService.kt
+++ app/src/main/java/com/commit451/gitlab/api/GitLabService.kt
@@ -17,6 +17,7 @@ interface GitLabService {
 
     companion object {
         const val API_VERSION = "api/v4"
+        const val TREE_PER_PAGE = "100"
     }
 
     /* --- LOGIN --- */
@@ -289,7 +290,7 @@ interface GitLabService {
     @GET(API_VERSION + "/projects/{id}/repository/contributors")
     fun getContributors(@Path("id") projectId: String): Single<List<Contributor>>
 
-    @GET(API_VERSION + "/projects/{id}/repository/tree?per_page=1000")
+    @GET(API_VERSION + "/projects/{id}/repository/tree?per_page=" + TREE_PER_PAGE)
     fun getTree(@Path("id") projectId: Long,
                 @Query("ref_name") branchName: String?,
                 @Query("path") path: String?): Single<List<RepositoryTreeObject>>
