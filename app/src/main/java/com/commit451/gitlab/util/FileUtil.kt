04c15f85d54d7ec0db116770e0c3312b9b81ba26
Markus Fußenegger <fussenegger@mobile-software.ag>
2019-04-01 14:01:06 +0200

    added UI to open files directly from the file list in an external app

--- app/src/main/java/com/commit451/gitlab/util/FileUtil.kt
+++ app/src/main/java/com/commit451/gitlab/util/FileUtil.kt
@@ -2,13 +2,27 @@ package com.commit451.gitlab.util
 
 
 import android.content.Context
+import android.content.Intent
 import android.graphics.Bitmap
 import android.net.Uri
+import android.os.AsyncTask
 import android.os.Environment
 import android.provider.MediaStore
 import android.provider.OpenableColumns
+import android.view.View
+import android.webkit.MimeTypeMap
 import androidx.core.content.FileProvider
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.commit451.gitlab.App
+import com.commit451.gitlab.R
+import com.commit451.gitlab.activity.FileActivity
+import com.commit451.gitlab.extension.base64Decode
+import com.commit451.gitlab.extension.with
+import com.commit451.gitlab.model.api.RepositoryFile
+import com.commit451.gitlab.rx.CustomSingleObserver
 import com.commit451.okyo.Okyo
+import com.google.android.material.snackbar.Snackbar
 import okhttp3.MediaType
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -95,4 +109,113 @@ object FileUtil {
         val state = Environment.getExternalStorageState()
         return Environment.MEDIA_MOUNTED == state
     }
+
+    fun open(context: Context, projectId : Long, path : String, ref : String) : LiveData<Boolean> {
+
+        val data = MutableLiveData<Boolean>()
+        val callback = MutableLiveData<DownloadResult>()
+        callback.observeForever { result ->
+
+            if(result != null){
+
+                val f = FileUtil.saveBlobToProviderDirectory(context, result.data, result.file.fileName!!)
+                openFile(context, f)
+                data.postValue(true)
+
+            } else {
+                data.postValue(false)
+            }
+
+        }
+
+        DownloadTask(projectId, path, ref, callback).execute()
+        return data
+
+    }
+
+    private class DownloadTask(private val projectId : Long, private val path : String, private val ref : String, private val callback : MutableLiveData<DownloadResult> ) : AsyncTask<Any, Any, DownloadResult>() {
+
+        override fun doInBackground(vararg params: Any?): DownloadResult {
+            val repoFile = App.get().gitLab.getFile(projectId, path, ref).blockingGet()
+            val blob = repoFile.content.base64Decode().blockingGet()
+            return DownloadResult(repoFile, blob)
+        }
+
+        override fun onPostExecute(result: DownloadResult?) {
+            callback.postValue(result)
+        }
+
+    }
+
+    private data class DownloadResult(val file: RepositoryFile, val data : ByteArray)
+
+    fun download(context: Context, projectId : Long, path : String, ref : String) : LiveData<RepositoryFile> {
+
+        val data = MutableLiveData<RepositoryFile>()
+        App.get().gitLab.getFile(projectId, path, ref)
+                .subscribe(object : CustomSingleObserver<RepositoryFile>() {
+
+                    override fun error(t: Throwable) {
+                        Timber.e(t)
+                        data.postValue(null)
+                    }
+
+                    override fun success(repositoryFile: RepositoryFile) {
+                        data.postValue(repositoryFile)
+                    }
+                })
+
+        return data
+
+    }
+
+    fun loadBlob(context: Context, file : RepositoryFile) : LiveData<ByteArray> {
+
+        val data = MutableLiveData<ByteArray>()
+        file.content.base64Decode().subscribe(object : CustomSingleObserver<ByteArray>() {
+
+                    override fun error(t: Throwable) {
+                        data.postValue(null)
+                    }
+
+                    override fun success(bytes: ByteArray) {
+                        data.postValue(bytes)
+                    }
+                })
+
+        return data
+
+    }
+
+    fun openFile(context: Context, fileName: String, blob : ByteArray) : Boolean {
+
+
+        val file = FileUtil.saveBlobToProviderDirectory(context, blob, fileName)
+        return openFile(context, file)
+
+    }
+
+    fun openFile(context: Context, file: File) : Boolean {
+
+        val intent = Intent(Intent.ACTION_VIEW)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+
+        val extension = FileActivity.fileExtension(file.absolutePath)
+        if (extension.isNotEmpty()) {
+            intent.type = MimeTypeMap.getSingleton().getMimeTypeFromExtension(extension)
+        }
+        intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+        intent.data = FileUtil.uriForFile(context, file)
+
+        return try {
+            context.startActivity(intent)
+            true
+        } catch (e: Exception) {
+            Timber.e(e)
+            false
+        }
+
+    }
+
+
 }
