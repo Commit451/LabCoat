dfd9d6b3cb26b4f4209a17ddf034b8846f3efa32
John <jawnnypoo@gmail.com>
2020-05-29 03:15:36 -0500

    Bump

--- buildSrc/src/main/kotlin/BuildHelper.kt
+++ buildSrc/src/main/kotlin/BuildHelper.kt
@@ -7,7 +7,7 @@ object BuildHelper {
     private var locatedFile: Boolean? = null
 
     fun appVersionName(): String {
-        return "2.7.3"
+        return "2.7.4"
     }
 
     fun appVersionCode(): Int {
